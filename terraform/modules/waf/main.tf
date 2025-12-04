9be817979c7280dd8ffd33417a06d8c905530138
Partho M <parthomazumdar999@gmail.com>
2025-12-04 23:10:20 +0530

    surecsuccess8

--- terraform/modules/waf/main.tf
+++ terraform/modules/waf/main.tf
@@ -12,6 +12,32 @@ resource "aws_wafv2_web_acl" "this" {
     metric_name                = "${var.environment}-waf"
     sampled_requests_enabled   = true
   }
-}  // till here you need to set the WAF rule manually from the portal//
 
-
+  rule {
+    name     = "BlockXSS"
+    priority = 1
+    action {
+      block {}
+    }
+    statement {
+      xss_match_statement {
+        field_to_match {
+          query_string {}
+        }
+        text_transformation {
+          priority = 0
+          type     = "URL_DECODE"
+        }
+        text_transformation {
+          priority = 1
+          type     = "HTML_ENTITY_DECODE"
+        }
+      }
+    }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "BlockXSS"
+      sampled_requests_enabled   = true
+    }
+  }
+}
